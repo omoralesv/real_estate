be38674ca50585ad26c324798fd4e37948ad51bd
Omar Morales <omarmv101@yahoo.com>
2023-02-09 20:50:02 -0800

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import { Box, Flex,HStack } from "@chakra-ui/react";
+import {AiOutlineMail, AiOutlineFacebook, AiOutlineInstagram,AiOutlinePhone} from 'react-icons/ai'
+
+const Footer = () =>(
+    <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100' fontSize='lg'>
+        2023 Jim Boonie's "Free" Real Estate, Inc
+        <HStack spacing='10px' justifyContent='center'>
+            <AiOutlineMail />
+            <AiOutlineFacebook/> 
+            <AiOutlineInstagram/> 
+            <AiOutlinePhone/>
+        </HStack>
+    </Box>
+)
+
+export default Footer 
